838d3e33ab915dedf4e35a655b49deaa73b16932
Michael Rosario <michael.p.rosario@gmail.com>
2022-01-27 10:18:28 -0500

    feat ( program ) integrate db

--- QA.app/Program.cs
+++ QA.app/Program.cs
@@ -1,7 +1,21 @@
+
+using DocumentStore.Core.Interfaces;
+using DocumentStore.Core.Services.DocumentStore.Core.Services;
+using Havit.Blazor.Components.Web;            
+using Havit.Blazor.Components.Web.Bootstrap;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Havit.Blazor.Components.Web;            
-using Havit.Blazor.Components.Web.Bootstrap;  
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using QA.Infra;
+using QA.Core.Entities;
+using QA.Core.Services;
+using System;
+using System.Reflection;
+using System.Text;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +23,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddHxServices(); 
+
+// infra ...
+var connectionString = builder.Configuration.GetConnectionString("DefaultPgConnection");
+var migrationsAssembly = typeof(QAContext).GetTypeInfo().Assembly.GetName().Name;
+builder.Services.AddDbContext<QAContext>(options =>
+{
+    options.UseNpgsql(connectionString, opt => opt.MigrationsAssembly(migrationsAssembly));
+});
+builder.Services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+builder.Services.AddScoped(typeof(IQAQueryRepository), typeof(QAQueryRepository));
+
+// core services ...
+builder.Services.AddScoped(typeof(IDocumentsService<>), typeof(DocumentsService<>));
 
 var app = builder.Build();
 
