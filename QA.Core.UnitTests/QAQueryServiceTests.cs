08f56f35d0094685e3cebe53f65242576767d0f3
Michael Rosario <michael.p.rosario@gmail.com>
2022-01-27 00:08:56 -0500

    feat ( query tests ) added another method

--- QA.Core.UnitTests/QAQueryServiceTests.cs
+++ QA.Core.UnitTests/QAQueryServiceTests.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 using QA.Core.Entities;
 using QA.Core.Services;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace QA.Core.UnitTests{
@@ -11,32 +12,74 @@ namespace QA.Core.UnitTests{
     [TestFixture]
     public class QAQueryServiceTests
     {
-        IQAQueryRepository repo;
-        IRepository<Question> questionRepo;
+        IQAQueryRepository _queryRepo;
+        IRepository<Question> _questionsRepo;
+        
 
         [SetUp]
         public void SetupStuff(){
-            repo = Substitute.For<IQAQueryRepository>();
-            questionRepo = Substitute.For<IRepository<Question>>();
+            _queryRepo = Substitute.For<IQAQueryRepository>();
+            _questionsRepo = Substitute.For<IRepository<Question>>();
         }
 
         [Test]
         public void QAQueryService__GetQuestions__TestHappyCase()
         {
             // arrange
-            var query = new QA.Core.Services.GetQuestionsQuery();
+            var query = new GetQuestionsQuery();
             query.Keyword = "test";
 
             var response2 = new GetQuestionsResponse();
-            repo.GetQuestions(query).Returns(response2);
+            _queryRepo.GetQuestions(query).Returns(response2);
 
-            var service = new QA.Core.Services.QAQueryService(repo,questionRepo);
+            var service = new QAQueryService(_queryRepo,_questionsRepo);
 
             // act
             var response = service.GetQuestions(query);
 
             // assert    
             Assert.NotNull(response);
+        }
+
+        [Test]
+        public void QAQueryService__GetQuestionAndAnswer__TestHappyCase()
+        {
+            // arrange
+            var service = new QAQueryService(_queryRepo, _questionsRepo);
+
+            var query = new DocumentStore.Core.Requests.GetDocumentQuery
+            {
+                UserId = "mrosario",
+                Id = "testId"
+            };
+
+            Question question = GetQuestion();
+
+            // setup repos ...
+            _questionsRepo.GetById(query.Id).Returns(Task.FromResult(question));
+            _queryRepo.GetAnswersForQuestion(query.Id).Returns(new List<QuestionAnswer>());
+
+            // act
+            var response = service.GetQuestionAndAnswers(query);
+
+            // assert    
+            Assert.NotNull(response);
+        }
+
+        private static Question GetQuestion()
+        {
+            return new Question()
+            {
+                Name = "what is 1 + 1 ?",
+                Content = "Need help with math",
+                HtmlContent = "Need help with math",
+                Tags = "tag1,tag2,tag3",
+                PermaLink = "permaLink",
+                CreatedAt = DateTime.Now,
+                CreatedBy = "mrosario",
+                Id = Guid.NewGuid().ToString(),
+                Abstract = "abstract"
+            };
         }
     }
 }
