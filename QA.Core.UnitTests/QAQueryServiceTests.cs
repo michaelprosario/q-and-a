616c4f00a0b7031a9a503b0968190d0936906ef3
michaelprosario <michael.p.rosario@gmail.com>
2022-01-27 04:43:57 +0000

    feat ( tests ) draft 1

--- QA.Core.UnitTests/QAQueryServiceTests.cs
+++ QA.Core.UnitTests/QAQueryServiceTests.cs
@@ -11,18 +11,26 @@ namespace QA.Core.UnitTests{
     [TestFixture]
     public class QAQueryServiceTests
     {
+        IQAQueryRepository repo;
+        IRepository<Question> questionRepo;
+
+        [SetUp]
+        public void SetupStuff(){
+            repo = Substitute.For<IQAQueryRepository>();
+            questionRepo = Substitute.For<IRepository<Question>>();
+        }
+
         [Test]
         public void QAQueryService__GetQuestions__TestHappyCase()
         {
             // arrange
-            var repo = Substitute.For<IQAQueryRepository>();
-            var questionRepo = Substitute.For<IRepository<Question>>();
-            
-            var service = new QA.Core.Services.QAQueryService(repo,questionRepo);
             var query = new QA.Core.Services.GetQuestionsQuery();
+            query.Keyword = "test";
+
             var response2 = new GetQuestionsResponse();
             repo.GetQuestions(query).Returns(response2);
-            query.Keyword = "test";
+
+            var service = new QA.Core.Services.QAQueryService(repo,questionRepo);
 
             // act
             var response = service.GetQuestions(query);
